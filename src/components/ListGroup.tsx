a81ace880e2424fa104e899068807c18a243bb52
LuMN <wiwi777guyy@gmail.com>
2024-01-10 10:28:59 +0630

    using Fragment

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,14 @@
+import { Fragment } from "react";
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">Item One</li>
-      <li className="list-group-item">Item Two</li>
-      <li className="list-group-item">Item Three</li>
-    </ul>
+    <Fragment>
+        <h1>List</h1>
+        <ul className="list-group">
+          <li className="list-group-item">Item One</li>
+          <li className="list-group-item">Item Two</li>
+          <li className="list-group-item">Item Three</li>
+        </ul>
+    </Fragment>
   );
 }
 export default ListGroup;
