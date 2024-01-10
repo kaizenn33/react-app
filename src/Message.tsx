6a60b16d275cb9ba3e3afee303bb5c99cb0a6cb1
LuMN <wiwi777guyy@gmail.com>
2024-01-10 09:27:20 +0630

    nature of JSX

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,9 @@
 //PascalCasing, react expected casing style
 function Message(){
 //JSX JavaScript XML
- return <h1>Hello World</h1>;
+const MyName = 'Kaizen';
+if(MyName)
+ return <h1>Hello {MyName}</h1>;
+ return <h1>No Name Now</h1>;
 }
 export default Message;
