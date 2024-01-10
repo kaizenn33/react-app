2c97fb767f00aab6514bb5488adf74c6031e0104
LuMN <wiwi777guyy@gmail.com>
2024-01-10 10:22:04 +0630

    adding list-group

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,10 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">Item One</li>
+      <li className="list-group-item">Item Two</li>
+      <li className="list-group-item">Item Three</li>
+    </ul>
+  );
+}
+export default ListGroup;
